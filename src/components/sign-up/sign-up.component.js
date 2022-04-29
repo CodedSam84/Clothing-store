9a3b72b41f81ec2481a646f0011ac2e57ecc42b0
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-04-29 12:47:58 +0100

    Fix bug in form input component and styles

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -3,7 +3,9 @@ import {
   createAuthUserWithEmailAndPassword, 
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
+import "./sign-up.styles.scss"
 
 const defaultsFormFields = {
   displayName: "",
@@ -45,8 +47,9 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
 
       <form onSubmit={handleSubmit}>
         <FormInput 
@@ -84,8 +87,9 @@ const SignUp = () => {
           onChange={handleChange} 
           value={passwordConfirmation}
         />
-
-        <button type="submit">SIGNUP</button>
+        <Button type="submit">
+          SIGNUP
+        </Button>
       </form>
     </div>
   );
